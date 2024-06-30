d7d885a928e8c7c91cef65cb24fb5ffd11cbb36a
Umesh Singh <umeshsinghmehta4@gmail.com>
2024-06-30 11:34:12 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="logo">
+            <h1>
+            StudyBuddy
+            </h1>
+            </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Course</a></li>
+            <li><a href="#">Q&A</a></li>
+            <li><a href="#">Quiz</a></li>
+            <li><a href="#">Room</a></li>
+        </ul>
+        <div className="register">
+            <button className="login">Login</button>
+            <button className="signup">Signup</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
